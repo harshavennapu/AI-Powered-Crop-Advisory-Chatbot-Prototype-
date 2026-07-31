e17973cc0143b27835bd5b1d3d8b2beecc7a6a6d
harshavennapu <vennapuharsha@gmail.com>
2026-07-31 21:35:30 +0530

    Update Google OAuth callback configuration

--- back-end/config/passport.js
+++ back-end/config/passport.js
@@ -7,10 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-  process.env.NODE_ENV === "production"
-    ? " https://ai-powered-crop-advisory-chatbot-fa3i.onrender.com/api/auth/google/callback"
-    : "http://localhost:5000/api/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
